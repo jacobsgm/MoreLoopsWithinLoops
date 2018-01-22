c1da35732894c7a47b5ddfe46f19960766003f47
jacobsgm <jacobsgm@rose-hulman.edu>
2018-01-22 14:55:34 -0500

    aya

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Garrett Jacobs.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -49,9 +49,34 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # ------------------------------------------------------------------
+
+    original_x = rectangle.corner_1.x
+    original_y = rectangle.corner_1.y
+    original_x2 = rectangle.corner_2.x
+    original_y2 = rectangle.corner_2.y
+    x = original_x
+    y = original_y
+    x2 = original_x2
+    y2 = original_y2
+
+    change_x = x2 - x
+    change_y = y2 - y
+    for k in range(n):
+        for j in range(k+1):
+            new_rectanglecorner1 = rg.Point(x,y)
+            new_rectanglecorner2 = rg.Point(x2,y2)
+            new_rectangle = rg.Rectangle(new_rectanglecorner1,new_rectanglecorner2)
+            new_rectangle.attach_to(window)
+            window.render(0.1)
+            x = x - change_x
+            x2 = x2 - change_x
+        y2 = y2 - change_y
+        y = y - change_y
+        x= original_x+((k+1)*0.5*change_x)
+        x2 = original_x2+((k+1)*0.5*change_x)
 
 
 # ----------------------------------------------------------------------
